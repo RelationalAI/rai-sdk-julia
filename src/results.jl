92d0409ac681b44867a81e425fca8822b4bc5130
Nathan Daly <nhdaly@gmail.com>
2022-08-12 10:29:09 -0600

    undo unrelated bug fix

--- src/results.jl
+++ src/results.jl
@@ -82,8 +82,7 @@ function show_result(io::IO, rsp::TransactionResponse)
 
     for index in eachindex(rsp.metadata)
         println(io, rsp.metadata[index]["relationId"])
-        data = rsp.results[index][2]
-        tuples = isempty(data) ? [()] : zip(data...)
+        tuples = zip(rsp.results[index][2]...)
         # Reuse julia's array printing function to print this array of tuples.
         Base.print_array(io, collect(tuples))
 
