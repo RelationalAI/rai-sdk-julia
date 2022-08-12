3e220b6431ec974f6735d5669b014c507474188c
Nathan Daly <nathan.daly@relational.ai>
2022-08-12 10:40:33 -0600

    Update examples/create-database.jl
    Co-authored-by: ary <ary@users.noreply.github.com>

--- examples/create-database.jl
+++ examples/create-database.jl
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-# Create a database, and print the response
+# Create a database and print the response.
 
 using RAI: Context, HTTPError, load_config, create_database
 
