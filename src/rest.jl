4bd3faf1ec1ddcb907e2659b25f2683a316529ef
Mary McGrath <m.c.mcgrath13@gmail.com>
2023-05-04 17:18:15 -0400

    fix: update connection_limit per warning

--- src/rest.jl
+++ src/rest.jl
@@ -184,6 +184,6 @@ function request(
     isnothing(body) && (body = UInt8[])
     headers = _ensure_headers(headers)
     _authenticate!(ctx, headers)
-    opts = (;redirect = false, connection_limit = 4096)
+    opts = (;redirect = false, pool = HTTP.Pool(4096))
     return HTTP.request(method, url, headers; query = query, body = body, opts..., kw...)
 end
