c7c4b94cafd67cb48d7ee51d15e09dbee6f12800
Pete Vilter <pete.vilter@gmail.com>
2023-09-06 14:47:17 -0400

    use a channel to return events

--- examples/get-transaction-events.jl
+++ examples/get-transaction-events.jl
@@ -20,56 +20,11 @@ using HTTP
 
 include("parseargs.jl")
 
-function run(; id, profile, streams=50, queries_per_stream=3)
+function run(; id, profile)
     conf = load_config(; profile = profile)
     ctx = Context(conf)
     
-    # successes = 0
-    # failures = 0
-    
-    # Threads.@sync begin
-    #     for t in 1:streams
-    #         Threads.@spawn begin
-    #             for i in 1:queries_per_stream
-    #                 @info "starting $t:$i"
-    #                 try
-    #                     resp_timed = @timed get_with_exp_backoff(ctx, id, 1, t, i)
-    #                     resp = resp_timed.value
-    #                     @info(
-    #                         "finished $t:$i",
-    #                         resp_timed.time,
-    #                         length(resp.events),
-    #                     )
-    #                     successes += 1
-    #                 catch err
-    #                     @error "request $t:$i failed" err
-    #                     failures += 1
-    #                 end
-    #             end
-    #         end
-    #     end
-    # end
-    
-    # return (; successes, failures)
     return get_transaction_events(ctx, id)
-end
-
-max_delay = 10
-
-function get_with_exp_backoff(ctx, id, attempt, t, i)
-    try
-        return get_transaction_events(ctx, id)
-    catch err
-        if err isa HTTPError && err.status_code == 429
-            sleep_dur = min(max_delay, 2 ^ attempt) + rand()
-            @info "backoff $t:$i for $sleep_dur sec"
-            sleep(sleep_dur)
-            get_with_exp_backoff(ctx, id, attempt + 1, t, i)
-        else
-            rethrow(err)
-            failures += 1
-        end
-    end
 end
 
 function main()
